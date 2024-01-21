5353e278140e3def649366580a996d40dd5e6db3
seeonkim <03essu@naver.com>
2024-01-21 10:34:41 +0900

    feat:0121

--- OOP/Repository/ProductRepository.cs
+++ OOP/Repository/ProductRepository.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Data;
 using OOP.Infra;
+using System.Random;
 
 namespace OOP.Repository {
     interface IProductRepository {
-        string createProductId();
+        int createProductId();
         void createProduct(string title, int price, string content, string sellerEmail);
-        object findProductById(string productId);
+        object findProductById(int productId);
         ArrayList findSellerProductsByEmail(string email);
         ArrayList findSellingProducts();
     }
@@ -17,16 +18,29 @@ namespace OOP.Repository {
         public ProductRepository() {
             this.db = Database.Instance;
         }
+        
+        private Random random = new Random();
+        public int createProductId()
+        {
+            int productId = random.Next(0, 10000);
+            var product = findProductById(productId);
+        
+            if (product != null)
+            {
+                return createProductId();
+            }
 
-        public string createProductId() {
-            throw new System.NotImplementedException();
+            return productId;
         }
-
+        
         public void createProduct(string title, int price, string content, string sellerEmail) {
-            throw new System.NotImplementedException();
+            int productId = createProductId();
+            bool isSelling = true;
+            string buyerEmail = null;
+            this.db.Write();
         }
 
-        public object findProductById(string productId) {
+        public object findProductById(int productId) {
             throw new System.NotImplementedException();
         }
 
--- OOP/Service/ProductService.cs
+++ OOP/Service/ProductService.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using OOP.Domain;
+using OOP.Repository;
 
 namespace OOP.Service {
     interface IProductService {
         List<ProductDto> GetMySellingProducts(string email);
-        void AddSellingProduct(string title, int price, string content, string sellerEmail);
+        bool AddSellingProduct(string title, int price, string content, string sellerEmail);
         object BuyProduct(string email, string productId);
     }
 
     internal class ProductService : IProductService {
+        private readonly IProductRepository _productRepository;
+        
         public List<ProductDto> GetMySellingProducts(string email) {
             return new List<ProductDto>();
         }
 
-        public void AddSellingProduct(string title, int price, string content, string sellerEmail) {
-            throw new System.NotImplementedException();
+        public bool AddSellingProduct(string title, int price, string content, string sellerEmail) {
+            this._productRepository.createProduct(title, price, content, sellerEmail);
+            return true;
         }
 
         public object BuyProduct(string email, string productId) {
